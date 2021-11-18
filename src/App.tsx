92903de745a8b906cedc2ee0296a3b93be94a8c2
gouster1 <94331446+gouster1@users.noreply.github.com>
2021-11-18 11:06:28 +0900

    test: codespace commit test

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,32 @@ import React from "react";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="container">
-			<header>헤더 공간</header>
-			<nav>네비게이션 공간</nav>
-			<aside>어사이드 공간</aside>
-			<main>
-				<article>
-					<h1>TEST</h1>
-					<ul>
-						<li>테스트1</li>
-						<li>테스트2</li>
-						<li>테스트3</li>
-					</ul>
-				</article>
-			</main>
-			<footer>
-				푸터 공간
-				<address>ddd</address>
-			</footer>
-		</div>
-	);
+  return (
+    <div className="container">
+      <header className="head">
+        헤더
+        <ruby>
+          공간<rt>space</rt>
+        </ruby>
+      </header>
+      <nav>네비게이션 공간</nav>
+      <aside>어사이드 공간</aside>
+      <main>
+        <article>
+          <h1>TEST</h1>
+          <ul>
+            <li>테스트1</li>
+            <li>테스트2</li>
+            <li>테스트3</li>
+          </ul>
+        </article>
+      </main>
+      <footer>
+        푸터 공간
+        <address>ddd</address>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
