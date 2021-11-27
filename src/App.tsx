3cd346b17e9995908edfc69f872754fef30b73f8
gouster1 <gouster147896325@gmail.com>
2021-11-27 19:53:50 +0900

    design: 성배 레이아웃 기본 구성

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,44 @@ function App() {
 			<header className="header">
 				<h1>헤더 공간</h1>
 			</header>
-			<nav className="nav">
+			<nav className="header_nav">
 				<button>1</button>
 				<button>2</button>
 				<button>3</button>
 				<button>4</button>
 				<button>5</button>
 			</nav>
-			<main className="main">
-				<aside className="aside">어사이드 공간</aside>
-				<article>
-					<h1>TEST</h1>
+
+			<div className="under_header">
+				<div className="side_space"></div>
+				<aside className="aside">
+					<h2>어사이드 공간</h2>
 					<ul>
-						<li>
-							테스트1
-							<br />
-							다음 줄
-						</li>
-						<li>테스트2</li>
-						<li>테스트3</li>
+						<li>첫번째</li>
+						<li>두번째</li>
+						<li>세번째</li>
 					</ul>
-				</article>
-			</main>
+				</aside>
+				<main className="main">
+					<article>
+						<h1>TEST</h1>
+						<ul>
+							<li>
+								테스트1
+								<br />
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+								Voluptas, atque, ut nesciunt aliquid iure architecto iusto quia
+								fugit ullam pariatur neque aspernatur veniam eum impedit
+								asperiores, libero distinctio. Labore, maxime.
+							</li>
+						</ul>
+					</article>
+				</main>
+				<div className="side_space"></div>
+			</div>
+
 			<footer className="footer">
-				푸터 공간
+				<h1>푸터 공간</h1>
 				<address>ddd</address>
 			</footer>
 		</div>
