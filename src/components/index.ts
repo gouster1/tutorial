03bef2922da8be869db15a363371c1d177a9f33f
gouster1 <gouster147896325@gmail.com>
2021-11-30 18:36:06 +0900

    index

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Header from "./organisms/header/header";
+import Nav from "./organisms/nav/nav";
+import Aside from "./organisms/aside/aside";
+import Main from "./organisms/main/main";
+import Footer from "./organisms/footer/footer";
